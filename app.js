608dd8551c303c1844a2a36fd2898778bb75bb28
adelinel <adeline.latruwe@gmail.com>
2020-11-09 10:28:37 -0800

    created function stubs to implement

--- app.js
+++ app.js
@@ -34,12 +34,14 @@ const store = {
  * 
  * Technical requirements:
  * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * Your app should include a render() function, that regenerates the view 
+ * each time the store is updated. 
  * See your course material and access support for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
  *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, 
+ * or link stylesheets or additional scripts if necessary
  *
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
  * 
@@ -49,10 +51,41 @@ const store = {
 
 // These functions return HTML templates
 
+// generate the start screen:
+// function startScreenHtml()
+
+// generate the quiz:
+// function questionNumberHtml()
+// function questionHtml()
+// function answerListHtml()
+// function scoreHtml()
+// function answerHtml()
+
+// generate the end screen:
+// function resultsScreenHtml()
+
 /********** RENDER FUNCTION(S) **********/
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+// This function conditionally replaces the contents of the <main> tag 
+//based on the state of the store
+
+// function renderScreen()
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+// function handleStartClick()
+// function handleSubmitAnswer()
+// function handleNextQuestion()
+// function handleRestartQuiz()
+
+
+/* function handleQuizApp() {
+  renderScreen()
+  function handleStartClick()
+  function handleSubmitAnswer()
+  function handleNextQuestion()
+  function handleRestartQuiz()
+} */
+
+// $(handleQuizApp);
